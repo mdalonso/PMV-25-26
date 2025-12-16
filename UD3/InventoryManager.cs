9d0e9e43a892babb7a1b808e4950b50345bc7688
mdalonso <38704285+mdalonso@users.noreply.github.com>
2025-12-16 13:53:14 +0100

    Enhance InventoryManager with new features
    Added methods to sum armor values and list unique item types.

--- UD3/InventoryManager.cs
+++ UD3/InventoryManager.cs
@@ -13,17 +13,17 @@ public class InventoryManager : MonoBehaviour
     {
         // Crear inventario de ejemplo
         inventario.Add(new Item("Espada", "Arma", 150));
-        inventario.Add(new Item("Poción de Vida", "Poción", 50));
+        inventario.Add(new Item("PociÃ³n de Vida", "PociÃ³n", 50));
         inventario.Add(new Item("Arco", "Arma", 120));
         inventario.Add(new Item("Escudo", "Armadura", 100));
-        inventario.Add(new Item("Poción de Maná", "Poción", 55));
+        inventario.Add(new Item("PociÃ³n de ManÃ¡", "PociÃ³n", 55));
         inventario.Add(new Item("Espada larga", "Arma", 200));
         inventario.Add(new Item("Cota de malla", "Armadura", 100));
-        inventario.Add(new Item("Poción de inmortalidad temporal", "Poción", 85));
+        inventario.Add(new Item("PociÃ³n de inmortalidad temporal", "PociÃ³n", 85));
         inventario.Add(new Item("Daga", "Arma", 75));
         inventario.Add(new Item("Casco", "Armadura", 100));
         inventario.Add(new Item("Piedra", "Arma", 50));
-        inventario.Add(new Item("Poción Venenosa", "Poción", 80));
+        inventario.Add(new Item("PociÃ³n Venenosa", "PociÃ³n", 80));
 
         MostrarInventario(inventario);
     }
@@ -41,7 +41,7 @@ public class InventoryManager : MonoBehaviour
     // Botones
     public void FiltrarPociones()
     {
-        var pociones = inventario.Where(i => i.Type == "Poción");
+        var pociones = inventario.Where(i => i.Type == "PociÃ³n");
          MostrarInventario(pociones);
     }
 
@@ -64,6 +64,33 @@ public class InventoryManager : MonoBehaviour
             }
         }
     }
+    public void SumarArmaduras()
+    {
+        int totalArmaduras = inventario
+            .Where(item => item.Type == "Armadura")  // Filtra solo las armaduras
+            .Sum(item => item.Value);                // Suma el valor de cada una
+        displayText.text = "Valor total de todas las armaduras: " + totalArmaduras;
+
+    }
+
+    public void Tipos()
+    {
+  
+        // Obtener los tipos Ãºnicos
+        List<string> tiposUnicos = inventario
+            .Select(item => item.Type)  // Selecciona solo la propiedad Type
+            .Distinct()                 // Elimina duplicados
+            .ToList();                  // Convierte a lista
+    
+        // Mostrar resultados
+        displayText.text = "--- TIPOS DE OBJETOS ---\n";
+        foreach (var tipo in tiposUnicos)
+        {
+             displayText.text += $"{tipo}\n";  
+        }
+
+    }
 }
+
 
 
