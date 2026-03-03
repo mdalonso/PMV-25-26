8a5d11d15e10f03acf3b3e44e3862f44341cc67a
mdalonso <38704285+mdalonso@users.noreply.github.com>
2026-03-03 13:51:12 +0100

    Add files via upload

--- UD4/Assets/Scripts/Managers/SpawnSystem/EnemyFactory.cs
+++ UD4/Assets/Scripts/Managers/SpawnSystem/EnemyFactory.cs
@@ -1,9 +1,11 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public static class EnemyFactory
 {
     private static float factor = 0.1f;//Factor para acotar la dificultad en el rango 0-1
+
+    [SerializeField] private static int _basicHealth = 1;//Vida del enemigo básico
+    [SerializeField] private static int _strongHealth = 3;//Vidal del enemigo Fuerte
     public static ObjectPoolBehaviour CreateRandomEnemy(int difficulty)
     {
 
@@ -13,6 +15,21 @@ public static class EnemyFactory
 
         ObjectPoolBehaviour enemy = PoolManager.Instance.GetFromPool(type); 
 
+        //Cuando sacamos un enemigo del pool hay que restablecer sus valores por defecto
+        ResetEnemy(enemy);
+
         return enemy;
+    }
+
+    static void ResetEnemy(ObjectPoolBehaviour enemy)
+    {
+        if (enemy.Type == PoolObjectType.BasicEnemy)
+        {
+            enemy.gameObject.GetComponent<EnemyHealth>().Health = _basicHealth;
+        }
+        else
+        {
+            enemy.gameObject.GetComponent<EnemyHealth>().Health = _strongHealth;
+        }
     }
 }
