f8d7c8f95b2e1961ba53dfd4bc87c66a9a48ffce
mdalonso <38704285+mdalonso@users.noreply.github.com>
2026-04-07 17:45:58 +0200

    Add files via upload

--- UD4/Assets/Scripts/Player/PlayerHealth.cs
+++ UD4/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] GameManagerSO _gameManager;
     [SerializeField] int _health=1;
+    [SerializeField] bool invulnerability=false;
+    int invulnerableDelay = 3;
 
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] float blinkRate = 0.1f;
+
+    [SerializeField] CameraController _cameraController;
     public int Health { get => _health;
         set
         {
@@ -17,11 +24,21 @@ public class PlayerHealth : MonoBehaviour
             }
         } 
     }
+    private void Start()
+    {
+        //Mostramos la vida del player en el HUD al iniciar el juego
+        UIManager.Instance.UpdateUIHealth(_health);
+    }
 
-   
     public void TakeDamage()
     {
+        if (invulnerability) return;//Si es invulnerable no se produce daño
+
         Health--;
+        invulnerability = true;
+        _cameraController.Shake();
+        StartCoroutine(MakeVulnerableAgain());
+        
     }
 
     void Die()
@@ -31,14 +48,41 @@ public class PlayerHealth : MonoBehaviour
         UIManager.Instance.ShowGameOverScreen();
         //ExitGame();//Reto 3
     }
-//Implementación de método para terminar la ejecución del juego en caso de que el player muera
-//Esta funcionalidad se integra en el GameManagerSO en base al Reto 4
-//    public void ExitGame() //Reto 3
-//    {
-//#if UNITY_EDITOR
-//        UnityEditor.EditorApplication.isPlaying = false;
-//#else
-//        Application.Quit();
-//#endif
-//    }
+    IEnumerator MakeVulnerableAgain()
+    {
+        StartCoroutine(BlinkRoutine());//Parpadeo mientras el Player es invulnerable
+        yield return new WaitForSeconds(invulnerableDelay);//Esperamos un tiempo y...
+        invulnerability = false;//...retiramos la invulnerabilidad del Player haciéndolo vulnerable de nuevo.
+    }
+    IEnumerator BlinkRoutine()
+    {
+        Debug.Log("blink");
+
+       
+        int t = 15;//Veces que va a parpadear. Permite gestionar la condición de salida de la corutina (OJO, NÚMERO MÁGICO)
+        while (t > 0)
+        {
+            spriteRenderer.enabled = false;//Desactivamos el Sprite Renderer...
+                                           //Para avisar visualmente al usuario de que el parpadeo va a finalizar, este parpadeo será cada vez más
+                                           //rápido. Para ello utilizamos una variable blinkRate que, multiplicada por t (número de veces que va a parpadear)
+                                           //cuyo valor es decreciente, hará que el tiempo que está desactivado el sprite renderer sea menor en cada parpadeo.
+            yield return new WaitForSeconds(t * blinkRate);//...esperamos x tiempo...
+            spriteRenderer.enabled = true;//...volvemos a activar el renderer...
+            yield return new WaitForSeconds(t * blinkRate);//...volvemos a esperar X tiempo antes de la siguiente iteración que volverá a desactivar el renderer
+            t--;
+
+        }
+        
+
+    }
+    //Implementación de método para terminar la ejecución del juego en caso de que el player muera
+    //Esta funcionalidad se integra en el GameManagerSO en base al Reto 4
+    //    public void ExitGame() //Reto 3
+    //    {
+    //#if UNITY_EDITOR
+    //        UnityEditor.EditorApplication.isPlaying = false;
+    //#else
+    //        Application.Quit();
+    //#endif
+    //    }
 }
